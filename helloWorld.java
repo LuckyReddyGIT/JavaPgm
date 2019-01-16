44ac4420bf4474f15fba78ad1cbc1d6f90f9b664
LuckyReddyGIT <lucky.y.reddy@accenture.com>
2019-01-16 12:34:28 +0000

    File modified

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld{
 	public static void main(String[] args){
-		System.out.println("Welcome to Java Jenkins Pgm");
+		System.out.println("Welcome to Hello World Prgm for Java Jenkins");
 	}
 }
